0b1ef55a2f8ee168ac683acce383b6cd98739c07
anaelkaleuavelino <anaelkaleu@gmail.com>
2025-06-05 17:55:20 -0300

    Commit inicial
    Arquivo inicial para projeto final de curso da Linguagem em Python, criação de lista de tarefas.

--- arquivo.py
+++ arquivo.py
@@ -1,19 +1,29 @@
+def adicionar_tarefa(lista_de_tarefas,tarefa):
+   lista_de_tarefas.append(tarefa)
+   print("Tarefa adcionada com sucesso!")
+   return lista_de_tarefas
+
+
 lista_de_tarefas = []
 continuar = True
 
 while continuar: 
     print("Escolha uma opcao:\n" \
           " 1 - Inserir nova tarefa\n" \
-          "2 - Lista de tarefas\n"
-        "3 - Sair")
+          " 2 - Lista de tarefas\n"
+          " 3 - Sair")
     opcao = input("insira o que deseja fazer: ")
     
 
     if opcao == "1":   
       tarefa = input('Insira uma nova tarefa')
-      lista_de_tarefas.append(tarefa)
+      lista_de_tarefas = adicionar_tarefa(tarefa)
+
     elif opcao == "2": 
-       print(lista_de_tarefas)
+       print("\nlista de tarefas")
+       for tarefa in lista_de_tarefas:
+          print(f"* {tarefa}")
+
     elif opcao == "3":
       continuar = False
     else:
